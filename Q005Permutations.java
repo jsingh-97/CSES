e0f92e733246958529d737179797ac2acff097e9
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-02-22 20:32:24 -0500

    optimizing permutations

--- Q005Permutations.java
+++ Q005Permutations.java
@@ -1,23 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Q005Permutations {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         if(n == 2 || n == 3){
             System.out.println("NO SOLUTION");
         }else{
             int counter = 2;
+            StringBuilder sb = new StringBuilder();
             while(counter <= n){
-                System.out.print(counter + " ");
+                sb.append(counter).append(" ");
                 counter += 2;
             }
             counter  = 1;
             while(counter <= n){
-                System.out.print(counter + " ");
+                sb.append(counter).append(" ");
                 counter += 2;
             }
-
+            System.out.print(sb);
         }
     }
 }
