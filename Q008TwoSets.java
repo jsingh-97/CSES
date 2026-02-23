cd81755515f0a5e1b6e20a95a1da64f992e0f945
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-02-22 20:41:21 -0500

    optimizing two sets

--- Q008TwoSets.java
+++ Q008TwoSets.java
@@ -1,36 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Q008TwoSets {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         long sumOfN = ((long) n * (n + 1))/2;
         if(sumOfN % 2 == 0){
-            System.out.println("YES");
-
-            long target = sumOfN / 2;
+            StringBuilder sb1 = new StringBuilder();
             int[] ans = new int[n + 1];
+            long target = sumOfN / 2;
             int countInSet = 0;
             for(int i = n; i > 0; i --){
                 if(target >= i) {
+                    sb1.append(i).append(" ");
                     ans[i] = 1;
                     target -= i;
                     countInSet++;
                 }
             }
-            System.out.println(countInSet);
-            for(int i = 1; i <= n; i++){
-                if(ans[i] == 1){
-                    System.out.print(i + " ");
-                }
-            }
-            System.out.println();
-            System.out.println(n - countInSet);
+            StringBuilder sb2 = new StringBuilder();
             for(int i = 1; i <= n; i++){
                 if(ans[i] == 0){
-                    System.out.print(i + " ");
+                    sb2.append(i).append(" ");
                 }
             }
+            System.out.println("YES");
+            System.out.println(countInSet);
+            System.out.println(sb1);
+            System.out.println(n - countInSet);
+            System.out.println(sb2);
         }else{
             System.out.print("NO");
         }
