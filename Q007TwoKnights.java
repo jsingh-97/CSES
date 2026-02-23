ca15e1b585197e02e5c3549f3d70e3a938425d1e
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-02-22 20:35:35 -0500

    problem solving

--- Q007TwoKnights.java
+++ Q007TwoKnights.java
@@ -7,11 +7,11 @@ public class Q007TwoKnights {
         for(int i = 1; i <= n; i++){
             long ans;
             long totalSquares = (long) i * i;
-            long totalWays = totalSquares * (totalSquares - 1);
-            long twoXThreeBlocks = (long) (i - 1) * (i - 2);
-            long threeXTwoBlocks = twoXThreeBlocks;
+            long totalWays = (totalSquares * (totalSquares - 1)) / 2;
+            long twoIntoThreeBlocks = (long) (i - 1) * (i - 2);
+            long threeIntoTwoBlocks = twoIntoThreeBlocks;
             //each 2 * 3 and 3 * 2 block has 2 positions where knights attack each other
-            ans = totalWays /2 - (2 * twoXThreeBlocks + 2 * threeXTwoBlocks);
+            ans = totalWays - 2 * (twoIntoThreeBlocks + threeIntoTwoBlocks);
             System.out.println(ans);
         }
     }
