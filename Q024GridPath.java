f7094c971695c4817d487db884dbbcb441718906
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-03-06 11:13:13 -0500

    Optimizing grid path

--- Q024GridPath.java
+++ Q024GridPath.java
@@ -1,71 +1,86 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Q024GridPath {
-    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static Map<Character, Integer> map = new HashMap<>();
     static int ans = 0;
-
-    private static void solve(String s, int idx, int i, int j, boolean[][] vis, int size) {
-        if ((i == (size - 1)) && j == 0) {
-            if (idx == s.length()) ans++;
+    private static void solve(char[] chs, int idx, int i, int j, boolean[][] vis){
+        if(i == 7 && j == 1){
+            if(idx == chs.length) ans++;
             return;
         }
-        if (idx == s.length()) return;
-        // Corridor split pruning, important and interesting
-        if (corridorSplitting(i, j, vis, size)) {
-            return;
+        if(idx == chs.length) return;
+        //prune the search
+        if(vis[i - 1][j] && vis[i + 1][j] && !vis[i][j - 1] &&!vis[i][j + 1]){
+            return ;
         }
-        if (s.charAt(idx) == '?') {
-            for (int[] dir : dirs) {
-                int nextI = i + dir[0];
-                int nextJ = j + dir[1];
-                if (nextI >= 0 && nextI < size && nextJ >= 0 && nextJ < size && !vis[nextI][nextJ]) {
-                    vis[nextI][nextJ] = true;
-                    solve(s, idx + 1, nextI, nextJ, vis, size);
-                    vis[nextI][nextJ] = false;
-                }
+        if(!vis[i - 1][j] && !vis[i + 1][j] && vis[i][j - 1] && vis[i][j + 1]){
+            return ;
+        }
+        if(chs[idx] == '?' || chs[idx] == 'D'){
+            int nextI = i + 1;
+            if(!vis[nextI][j]){
+                vis[nextI][j] = true;
+                solve(chs, idx + 1, nextI, j, vis);
+                vis[nextI][j] = false;
             }
-        } else {
-            int k = map.get(s.charAt(idx));
-            int nextI = i + dirs[k][0];
-            int nextJ = j + dirs[k][1];
-            if (nextI >= 0 && nextI < size && nextJ >= 0 && nextJ < size && !vis[nextI][nextJ]) {
-                vis[nextI][nextJ] = true;
-                solve(s, idx + 1, nextI, nextJ, vis, size);
-                vis[nextI][nextJ] = false;
+        }
+        if(chs[idx] == '?' || chs[idx] == 'U'){
+            int nextI = i - 1;
+            if(!vis[nextI][j]){
+                vis[nextI][j] = true;
+                solve(chs, idx + 1, nextI, j, vis);
+                vis[nextI][j] = false;
+            }
+        }
+        if(chs[idx] == '?' || chs[idx] == 'L'){
+            int nextJ = j - 1;
+            if(!vis[i][nextJ]){
+                vis[i][nextJ] = true;
+                solve(chs, idx + 1, i, nextJ, vis);
+                vis[i][nextJ] = false;
+            }
+        }
+        if(chs[idx] == '?' || chs[idx] == 'R'){
+            int nextJ = j + 1;
+            if(!vis[i][nextJ]){
+                vis[i][nextJ] = true;
+                solve(chs, idx + 1, i, nextJ, vis);
+                vis[i][nextJ] = false;
             }
         }
     }
 
-    private static boolean corridorSplitting(int i, int j, boolean[][] vis, int size) {
-        // Case 1: Up & Down blocked, Left & Right free
-        return ((
-                ((i == 0 || vis[i - 1][j]) && (i == size - 1 || vis[i + 1][j])) &&
-                        (j > 0 && !vis[i][j - 1]) &&
-                        (j < size - 1 && !vis[i][j + 1])
-        )
-                ||// Case 2: Left & Right blocked, Up & Down free
-                (
-                        ((j == 0 || vis[i][j - 1]) && (j == size - 1 || vis[i][j + 1])) &&
-                                (i > 0 && !vis[i - 1][j]) &&
-                                (i < size - 1 && !vis[i + 1][j])
-                ));
+    private static int getPtr(char c) {
+        switch (c) {
+            case 'D': {
+                return 0;
+            }
+            case 'U' : {
+                return 1;
+            }
+            case 'L' : {
+                return 2;
+            }
+            case 'R' : {
+                return 3;
+            }
+            default : {
+                return  -1;
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int size = 7;
-        boolean[][] vis = new boolean[size][size];
-        map.put('D', 0);
-        map.put('U', 1);
-        map.put('R', 2);
-        map.put('L', 3);
-        vis[0][0] = true;
-        solve(s, 0, 0, 0, vis, size);
-        System.out.print(ans);
+    public static void main(String [] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        boolean[][] vis = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            vis[0][i] = vis[8][i] = true;
+            vis[i][0] = vis[i][8] = true;
+        }
+        vis[1][1] = true;
+        solve(s.toCharArray(), 0, 1, 1, vis);
+        System.out.println(ans);
     }
 }
