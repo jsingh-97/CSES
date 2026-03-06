2ad81341f7c16638e5c3399b39144a5668e7f256
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-03-06 09:10:14 -0500

    Optimizing string reorder

--- Q023StringReorder.java
+++ Q023StringReorder.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
-import java.lang.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Q023StringReorder {
     private static boolean isPossible(int[] freq, char ch){
         freq[ch - 'A']--;
         int highestFreq = 0;
         int total = 0;
-        for(int i = 0; i < freq.length; i++){
-            highestFreq = Math.max(highestFreq, freq[i]);
-            total += freq[i];
+        for(int f: freq){
+            highestFreq = Math.max(highestFreq, f);
+            total += f;
         }
         freq[ch - 'A']++;
         return highestFreq <= ((total + 1) / 2);
     }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
         int[] freq = new int[26];
         for(char ch: s.toCharArray()){
             freq[ch - 'A']++;
@@ -39,9 +40,7 @@ public class Q023StringReorder {
         if(!answerPossible){
             System.out.println(-1);
         }else{
-            for(int i = 0; i < sb.length(); i++){
-                System.out.print(sb.charAt(i));
-            }
+            System.out.print(sb);
         }
 
     }
