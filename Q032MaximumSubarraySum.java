2f126dd041cdbf6d5774e510f1c8a177d15df2a0
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-03-22 14:35:12 -0400

    solve max subarray sum using kadane

---
+++ Q032MaximumSubarraySum.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Q032MaximumSubarraySum {
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c, sign = 1, val = 0;
+            do {
+                c = read();
+            } while (c <= ' ');//skip space, \n, \t
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+
+            return val * sign;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+       FastReader fr = new FastReader();
+        int n = fr.nextInt();
+        long maxsum = Long.MIN_VALUE, cursum = 0;
+        for(int i = 0; i < n; i++){
+            int val = fr.nextInt();
+            cursum = Math.max(cursum + val, val);
+            maxsum = Math.max(maxsum, cursum);
+        }
+        System.out.println(maxsum);
+    }
+}
