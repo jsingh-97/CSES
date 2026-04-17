be97384fdcd0c7d16516a4f58c3cba907725186a
Jaswinder Singh <jaswindervirk.jv.js@gmail.com>
2026-04-17 10:54:33 -0400

    rename

--- Q042JosephusProblemI.java
+++ Q042JosephusProblemI.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class JosephusProblemI {
+public class Q042JosephusProblemI {
 
     public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
